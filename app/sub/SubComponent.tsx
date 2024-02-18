fce247d88b7f8c0d592f828ce0ad3a3c858061bc
azu <azuciao@gmail.com>
2024-02-18 21:25:44 +0900

    Update app/css.ts+6

---
+++ app/sub/SubComponent.tsx
@@ -0,0 +1,11 @@
+import { css } from "@/app/css";
+
+export const SubComponent = () => {
+    return (
+        <div>
+            <h2 style={css({
+                color: "red"
+            })}>SubComponent</h2>
+        </div>
+    )
+}
---
+++ app/sub/page.tsx
@@ -0,0 +1,9 @@
+import { SubComponent } from "@/app/sub/SubComponent";
+
+export default function Page() {
+    return <div>Page Sub
+
+        <SubComponent/>
+    </div>
+
+}
