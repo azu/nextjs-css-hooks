d8b0b8a1c470d1a3f473e2ff7f97dc0fd5fd9151
azu <azuciao@gmail.com>
2024-02-18 21:30:22 +0900

    link

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,9 @@ export default function Home() {
                         }
                     })
                 }>Next.js App Router + CSS Hooks</h1>
-                <Link href={"sub"}>SuB Page</Link>
+                <Link href={"sub"} style={{
+                    color: "blue"
+                }}>SuB Page</Link>
                 <p>
                     Source: <a title="azu/nextjs-css-hooks: undefined" href="https://github.com/azu/nextjs-css-hooks">azu/nextjs-css-hooks</a>
                 </p>
